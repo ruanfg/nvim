b7fa3240658066eb8fadb1556892be94f50f67b3
ruanfeng <rf570787284@gmail.com>
2022-01-23 19:44:57 +0800

    add git some plugins

---
+++ init.lua
@@ -0,0 +1,14 @@
+require('init-packer')
+require('github-theme').setup()
+require('init-lsp')
+
+vim.o.number = true
+vim.o.relativenumber = true
+vim.o.tabstop = 4
+vim.o.shiftwidth = 4
+vim.o.expandtab = true
+vim.o.smartindent = true
+vim.o.termguicolors = true
+vim.o.cursorline = true
+vim.g.mapleader = ' '
+
---
+++ lua/init-packer.lua
@@ -0,0 +1,56 @@
+return require('packer').startup(function()
+  -- Packer can manage itself
+  use 'wbthomason/packer.nvim'
+
+  -- Github Theme
+  use "projekt0n/github-nvim-theme"
+
+  -- Collection of configurations for built-in LSP client
+  use 'neovim/nvim-lspconfig' 
+
+  -- Autocompletion plugin
+  use 'hrsh7th/nvim-cmp' 
+
+  -- LSP source for nvim-cmp
+  use 'hrsh7th/cmp-nvim-lsp' 
+
+  -- Snippets source for nvim-cmp
+  use 'saadparwaiz1/cmp_luasnip' 
+
+  -- Snippets plugin
+  use 'L3MON4D3/LuaSnip' 
+
+  -- A File Explorer For Neovim Written In Lua
+  use {
+      'kyazdani42/nvim-tree.lua',
+      requires = {
+          'kyazdani42/nvim-web-devicons', -- optional, for file icon
+      },
+      config = function() require'nvim-tree'.setup {
+          auto_close = true
+      } end
+  }
+
+  -- Tabline
+  use {
+      'akinsho/bufferline.nvim', 
+      requires = 'kyazdani42/nvim-web-devicons',
+      config = function() require("bufferline").setup{} end
+  }
+
+  -- Statusline
+  use {
+      'feline-nvim/feline.nvim',
+      config = function() require('feline').setup{} end
+  }
+
+  -- git signs
+  use {
+      'lewis6991/gitsigns.nvim',
+      requires = {
+          'nvim-lua/plenary.nvim'
+      },
+      config = function() require('gitsigns').setup() end
+  }
+
+end)
